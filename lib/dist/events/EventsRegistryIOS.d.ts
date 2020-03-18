417aa34a822c878e3e013386250b528d8c54f2a0
Antonio Moreno Valls <amorenovalls@gmail.com>
2020-03-18 12:13:09 +0100

    Compile files

---
+++ lib/dist/events/EventsRegistryIOS.d.ts
@@ -0,0 +1,11 @@
+import { EmitterSubscription } from 'react-native';
+import { NativeEventsReceiver } from '../adapters/NativeEventsReceiver';
+import { RegisteredPushKit } from '../interfaces/NotificationEvents';
+export declare class EventsRegistryIOS {
+    private nativeEventsReceiver;
+    constructor(nativeEventsReceiver: NativeEventsReceiver);
+    registerPushKitRegistered(callback: (event: RegisteredPushKit) => void): EmitterSubscription;
+    registerPushKitNotificationReceived(callback: (event: object) => void): EmitterSubscription;
+    registerOnStartCallAction(callback: (event: object) => void): EmitterSubscription;
+    registerOnEndCallAction(callback: (event: object) => void): EmitterSubscription;
+}
